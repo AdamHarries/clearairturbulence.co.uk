e781f35288ec1c3259a8a0327e1a2c1d3d8c7516
Adam Harries <adam.harries@codeplay.com>
2018-09-04 18:33:35 +0100

    remove separate travis deployment script, encapsulate in config

--- deploy_travis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-stack build
-stack exec site clean
-stack exec site build
-cp -r _site public
-rsync -r -e "ssh -i key.prv" public aharries_clearairturbulence@ssh.phx.nearlyfreespeech.net:/home/
-rm -rf public
